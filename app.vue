36b3b3815f0a79279fa236ae39920bd6e8a69b75
Paul <paul.hofbauer@oabat.de>
2024-06-26 23:07:52 +0200

    navigation

--- app.vue
+++ app.vue
@@ -1,5 +1,29 @@
 <template>
   <div>
+    <nav>
+      <NuxtLink :class="{'page-link': true, 'page-link--active': $route.path === '/'}" to="/">Sentence similarity</NuxtLink>
+      <NuxtLink :class="{'page-link': true, 'page-link--active': $route.path === '/fulltext'}" to="/fulltext">Fulltext search</NuxtLink>
+    </nav>
     <NuxtPage />
   </div>
 </template>
+
+
+<style>
+
+nav {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+}
+
+.page-link {
+  margin: 1rem;
+  color: black;
+}
+
+.page-link--active {
+  font-weight: bold;
+  text-decoration: underline;
+}
+</style>
